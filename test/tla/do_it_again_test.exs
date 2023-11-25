03dd6adb6f740e0f9c0261ec5f06dcf36e3b2af6
Jim Greer <jbgreer@gmail.com>
2023-11-25 10:20:00 -0600

    forcing commit

--- test/tla/do_it_again_test.exs
+++ test/tla/do_it_again_test.exs
@@ -9,12 +9,12 @@ defmodule TLA.DoItAgainTest do
     assert lat?([]) == true
   end
 
-  test "lat?([:foo, :bar]) == true" do
-    assert lat?([:foo, :bar]) == true
-  end
-
   test "lat?([ [:foo] ]) == false" do
     assert lat?([[:foo]]) == false
+  end
+
+  test "lat?([:foo, :bar]) == true" do
+    assert lat?([:foo, :bar]) == true
   end
 
   test "member?(:foo, [ :foo ] ) == true" do
