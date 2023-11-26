c68484d03055f94293e538301d8f008cd7164b5f
Jim Greer <jbgreer@gmail.com>
2023-11-26 16:47:29 -0600

    test commit

--- lib/the_little_alchemist.ex
+++ lib/the_little_alchemist.ex
@@ -1,4 +1,10 @@
 defmodule TheLittleAlchemist do
+  @moduledoc """
+  Created with mix new.  No real functions here.
+  """
+  @doc """
+  Returns :world atom
+  """
   def hello() do
     :world
   end
